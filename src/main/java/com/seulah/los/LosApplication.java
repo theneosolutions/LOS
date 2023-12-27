291fe48e5ca096d4414cfc96dfff1628a026c0d5
mansoor <Mansoor@theneosolutions.com>
2023-12-27 16:58:05 +0500

    add eureka client

--- src/main/java/com/seulah/los/LosApplication.java
+++ src/main/java/com/seulah/los/LosApplication.java
@@ -2,10 +2,12 @@ package com.seulah.los;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class LosApplication {
 
     public static void main(String[] args) {
